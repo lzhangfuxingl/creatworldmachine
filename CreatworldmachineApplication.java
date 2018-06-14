9da5f98492ffc624e47e94763bfd5ade603a50e3
zhangfx <123>
2018-06-14 17:27:47 +0800

    test1

---
+++ CreatworldmachineApplication.java
@@ -0,0 +1,12 @@
+package com.xxg.zfx.creatworldmachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatworldmachineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreatworldmachineApplication.class, args);
+    }
+}
